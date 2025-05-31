35c0b752e88c758de66963dde8db35f364f82391
Potti1234 <lukas.pottner@gmail.com>
2025-06-01 00:28:51 +0200

    Add real stream component

--- frontend/components/streamComponent.tsx
+++ frontend/components/streamComponent.tsx
@@ -1,34 +1,37 @@
 'use client'
 
 import React, { useState, useEffect, useRef } from 'react'
-import { WebRTCAdaptor } from '@antmedia/webrtc_adaptor'
+import { WebRTCAdaptor as WebRTCAdaptorType } from '@antmedia/webrtc_adaptor' // Alias the type
 import { Button } from '@/components/ui/button'
 import { createStream, deleteStream } from '@/lib/api-stream'
+import { toast } from 'sonner'
 
 const StreamComponent = () => {
   const [publishing, setPublishing] = useState(false)
   const [websocketConnected, setWebsocketConnected] = useState(false)
-  const [streamId, setStreamId] = useState('stream123')
-  const webRTCAdaptor = useRef<WebRTCAdaptor | null>(null)
+  const [streamId, setStreamId] = useState(
+    'stream123' + Math.random().toString(36).substring(2, 15)
+  )
+  const webRTCAdaptor = useRef<WebRTCAdaptorType | null>(null) // Use aliased type
   const publishingStream = useRef<string | null>(null)
 
   const handleCreateStream = async () => {
     const newStream = await createStream(streamId)
     if (newStream) {
-      alert(
+      toast.success(
         `Stream created with ID: ${newStream.id} and StreamId: ${newStream.streamId}`
       )
     } else {
-      alert('Failed to create stream.')
+      toast.error('Failed to create stream.')
     }
   }
 
   const handleDeleteStream = async () => {
     const success = await deleteStream(streamId)
     if (success) {
-      alert(`Stream with StreamId: ${streamId} deleted successfully.`)
+      toast.success(`Stream with StreamId: ${streamId} deleted successfully.`)
     } else {
-      alert(`Failed to delete stream with StreamId: ${streamId}.`)
+      toast.error(`Failed to delete stream with StreamId: ${streamId}.`)
     }
   }
 
@@ -55,41 +58,50 @@ const StreamComponent = () => {
   }
 
   useEffect(() => {
-    if (webRTCAdaptor.current === undefined || webRTCAdaptor.current === null) {
-      webRTCAdaptor.current = new WebRTCAdaptor({
-        websocket_url: 'wss://ams-30774.antmedia.cloud:5443/LiveApp/websocket', // Replace with your WebSocket URL
-        mediaConstraints: {
-          video: true,
-          audio: true
-        },
-        peerconnection_config: {
-          iceServers: [{ urls: 'stun:stun1.l.google.com:19302' }]
-        },
-        sdp_constraints: {
-          OfferToReceiveAudio: false, // Usually false for publishing
-          OfferToReceiveVideo: false // Usually false for publishing
-        },
-        localVideoId: 'localVideo', // Changed from remoteVideoId
-        callback: (info: any) => {
-          if (info === 'initialized') {
-            setWebsocketConnected(true)
-          }
-          if (info === 'publish_started') {
-            setPublishing(true)
-            console.log('publish started')
-          } else if (info === 'publish_finished') {
-            setPublishing(false)
-            console.log('publish finished')
-          }
-        },
-        callbackError: function (error: any, message: any) {
-          console.error('error callback: ' + JSON.stringify(error))
-          console.error('error message: ' + message)
-          // Re-try mechanism can be implemented here
-          if (error === 'publishTimeoutError') {
-            // You can try to republish.
-          }
-          setPublishing(false)
+    if (typeof window !== 'undefined') {
+      // Ensure this runs only on client-side
+      import('@antmedia/webrtc_adaptor').then(({ WebRTCAdaptor }) => {
+        if (
+          webRTCAdaptor.current === undefined ||
+          webRTCAdaptor.current === null
+        ) {
+          webRTCAdaptor.current = new WebRTCAdaptor({
+            websocket_url:
+              'wss://ams-30774.antmedia.cloud:5443/LiveApp/websocket', // Replace with your WebSocket URL
+            mediaConstraints: {
+              video: true,
+              audio: true
+            },
+            peerconnection_config: {
+              iceServers: [{ urls: 'stun:stun1.l.google.com:19302' }]
+            },
+            sdp_constraints: {
+              OfferToReceiveAudio: false, // Usually false for publishing
+              OfferToReceiveVideo: false // Usually false for publishing
+            },
+            localVideoId: 'localVideo', // Changed from remoteVideoId
+            callback: (info: any) => {
+              if (info === 'initialized') {
+                setWebsocketConnected(true)
+              }
+              if (info === 'publish_started') {
+                setPublishing(true)
+                console.log('publish started')
+              } else if (info === 'publish_finished') {
+                setPublishing(false)
+                console.log('publish finished')
+              }
+            },
+            callbackError: function (error: any, message: any) {
+              console.error('error callback: ' + JSON.stringify(error))
+              console.error('error message: ' + message)
+              // Re-try mechanism can be implemented here
+              if (error === 'publishTimeoutError') {
+                // You can try to republish.
+              }
+              setPublishing(false)
+            }
+          })
         }
       })
     }
@@ -97,8 +109,6 @@ const StreamComponent = () => {
 
   return (
     <div className='container mx-auto text-center p-4'>
-      <h1 className='text-3xl font-bold mb-8'>Stream Page</h1>
-
       <div className='mb-4'>
         <div className='flex justify-center'>
           <video
@@ -117,20 +127,6 @@ const StreamComponent = () => {
         </div>
       </div>
       <div className='flex flex-col items-center'>
-        <div className='mb-3 w-full max-w-md'>
-          <input
-            className='block w-full p-2 text-lg border border-gray-300 rounded-md'
-            type='text'
-            defaultValue={streamId}
-            onChange={handleStreamIdChange}
-          />
-          <label
-            className='block text-sm font-medium text-gray-700 mt-1'
-            htmlFor='streamId'
-          >
-            Enter Stream Id
-          </label>
-        </div>
         <div className='flex justify-center'>
           {!publishing ? (
             <Button
